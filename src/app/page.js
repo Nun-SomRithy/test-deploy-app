e7aa532561e62732a0dce0f12a4cb71331261618
chirifu_jumi <nunsomrithy098@gmail.com>
2023-08-26 00:10:23 +0700

    Can u see me

--- src/app/page.js
+++ src/app/page.js
@@ -97,14 +97,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Hello World{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          </p>
         </a>
       </div>
     </main>
