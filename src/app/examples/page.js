921f91e306f12b77e10d83883fea8a781556a981
NunSomRithy <nunsomrithy098@gmail.com>
2023-09-01 10:15:03 +0700

    add examples file

---
+++ src/app/examples/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page(props) {
+    return (
+        <div>
+            <h1 class="text-2xl bg-amber-300 text-center"> Hello Testing Reverse Proxy </h1>
+        </div>
+    );
+}
+
+export default Page;
